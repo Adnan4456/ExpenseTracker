52ac5f27849ea42383157d4d274b66e565bca269
adnan <adnan4456@gmail.com>
2024-05-04 14:57:29 +0200

    streak

--- app/src/main/java/com/example/expensetracker/feature_showExpense/presentation/ui/ExpensesScreen.kt
+++ app/src/main/java/com/example/expensetracker/feature_showExpense/presentation/ui/ExpensesScreen.kt
@@ -1,14 +1,35 @@
 package com.example.expensetracker.feature_showExpense.presentation.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.widget.Space
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Camera
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.expensetracker.common.topBar
-
+import com.example.expensetracker.R
+import com.example.expensetracker.ui.theme.expenseGradient
+import com.example.expensetracker.ui.theme.incomeGradient
+import kotlin.math.exp
 
 @Composable
 fun  ExpensesScreen(
@@ -21,8 +42,173 @@ fun  ExpensesScreen(
         Column(
             modifier = Modifier.fillMaxSize()
         ) {
-            Text(text =     viewModel.formattedDate.value)
-
+           Header(viewModel = viewModel)
         }
     }
+}
+
+@Composable
+fun Header(viewModel: HomeViewModel){
+    
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ){
+            Text(text = "")
+
+            Text(text = viewModel.formattedDate.value)
+
+            Icon(
+                imageVector = Icons.Filled.Notifications,
+                contentDescription = "")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = "Account Balance")
+        
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = "$98000",
+        style = TextStyle(
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 24.sp
+        )
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .height(70.dp)
+                    .width(150.dp)
+                    .clip(
+                        RoundedCornerShape(20)
+                    )
+                    .background(
+                        brush = incomeGradient
+                    )
+                    .padding(4.dp),
+                contentAlignment = Alignment.Center
+            ){
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(4.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .width(40.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                            .background(
+                                color = Color.White
+                            )
+                            .padding(horizontal = 8.dp, vertical = 4.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            modifier= Modifier.size(10.dp),
+                            painter = painterResource(id = R.drawable.arrow_downward),
+                            contentDescription = "",
+                        )
+                        Icon(
+                            modifier= Modifier.size(20.dp),
+                            painter = painterResource(id = R.drawable.camera),
+                            contentDescription = "",
+                        )
+                    }
+
+                    Column(
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Text(text = "income",
+                        style = TextStyle(
+                            fontSize = 12.sp,
+                        )
+                        )
+                        Text(text = "$100")
+                    }
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .height(70.dp)
+                    .width(150.dp)
+                    .clip(
+                        RoundedCornerShape(20)
+                    )
+                    .background(
+                        brush = expenseGradient
+                    )
+                    .padding(4.dp),
+                contentAlignment = Alignment.Center
+            ){
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(4.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .width(40.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                            .background(
+                                color = Color.White
+                            )
+                            .padding(horizontal = 8.dp, vertical = 4.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            modifier= Modifier.size(10.dp),
+                            painter = painterResource(id = R.drawable.arrow_upward),
+                            contentDescription = "",
+                        )
+                        Icon(
+                            modifier= Modifier.size(20.dp),
+                            painter = painterResource(id = R.drawable.camera),
+                            contentDescription = "",
+                        )
+                    }
+
+                    Column(
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Text(text = "Expense",
+                            style = TextStyle(
+                                fontSize = 12.sp,
+                            )
+                        )
+                        Text(text = "$100")
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Preview(){
+
 }
--- app/src/main/java/com/example/expensetracker/feature_showExpense/presentation/ui/viewModel.kt
+++ app/src/main/java/com/example/expensetracker/feature_showExpense/presentation/ui/viewModel.kt
@@ -2,19 +2,28 @@ package com.example.expensetracker.feature_showExpense.presentation.ui
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
 import com.example.expensetracker.domain.usecase.GetDateFormattedUseCase
 import com.example.expensetracker.domain.usecase.GetDateUseCase
+import com.example.expensetracker.domain.usecase.read_database.GetCurrentDayExpTranscationUseCase
+import com.example.expensetracker.domain.usecase.read_database.GetDailyTranscationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import java.security.PrivateKey
 import java.util.*
 import javax.inject.Inject
 
-
+import com.example.expensetracker.domain.model.Transcation
 @HiltViewModel
 class HomeViewModel
     @Inject constructor(
-     val  getDateUseCase:GetDateUseCase,
-     val getFormattedDateUseCase: GetDateFormattedUseCase,
+     private val  getDateUseCase:GetDateUseCase,
+     private val getFormattedDateUseCase: GetDateFormattedUseCase,
+     private val getCurrentDayExpTranscationUseCase: GetCurrentDayExpTranscationUseCase,
+     private val getDailyTransactionUseCase: GetDailyTranscationUseCase,
 ): ViewModel() {
 
     var currentTime = MutableStateFlow(Calendar.getInstance().time)
@@ -26,16 +35,24 @@ class HomeViewModel
     var date = MutableStateFlow(String())
         private set
 
+    var dailyTransaction = MutableStateFlow(emptyList<Transcation>())
+        private set
     init {
 
         val currentDate = getDateUseCase()
-        Log.d("TAG","current date = ${currentDate}")
+//        Log.d("TAG","current date = ${currentDate}")
         formattedDate.value = getFormattedDateUseCase(currentTime.value)
-        Log.d("TAG","formattedDate  = ${formattedDate.value}")
-        Log.d("TAG","Current Time  = ${currentTime.value}")
-
         date.value = currentDate
 
-
+        viewModelScope.launch {
+            getDailyTransactionUseCase.invoke(currentDate).collect{
+                it?.let {expense ->
+                    Log.d("TAG","$expense")
+                    dailyTransaction.value = expense.map {dailyExpense ->
+                        dailyExpense.toTranscation()
+                    }.reversed()
+                }
+            }
+        }
     }
 }
